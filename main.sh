408ee77946b2850a0ab8f866e75673e98ee0d646
Jcurt4 <joe.curtis.iv@gmail.com>
2024-09-18 21:44:02 -0600

    added first unit test and passed it

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-python3 public/src/main.py
+python3 src/main.py
---
+++ src/main.py
@@ -0,0 +1,26 @@
+from textnode import TextNode
+
+
+
+
+
+def main():
+    
+
+    text_node = TextNode("This is a test", "p", "http://www.google.com")
+    print(text_node.__repr__())
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()

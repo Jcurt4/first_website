989cb83448f2e2cfc837b601a57f6722b1ffd13b
Jcurt4 <joe.curtis.iv@gmail.com>
2024-09-18 20:54:41 -0600

    complete Ch.2 Lesson 1

--- public/src/main.py
+++ public/src/main.py
@@ -1,1 +1,26 @@
-print('hello world')
+from textnode import Textnode
+
+
+
+
+
+def main():
+    
+    
+    text_node = Textnode("This is a test", "p", "http://www.google.com")
+    print(text_node.__repr__())
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()

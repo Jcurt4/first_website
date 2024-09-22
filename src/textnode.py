bc5262dba9843650dbc278a0d2a4fbb632903b29
Jcurt4 <joe.curtis.iv@gmail.com>
2024-09-22 17:08:40 -0600

    added text node to html node func

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,7 @@
+from htmlnode import LeafNode
+
+
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -13,3 +17,21 @@ class TextNode:
                 self.url == other.url)
     def __repr__(self):
         return f"TextNode({self.text!r}, {self.text_type!r}, {self.url!r})"
+    
+
+    
+def text_node_to_html_node(text_node):
+    tag_map = {
+        "text": ("", text_node.text),
+        "bold": ("b", text_node.text),
+        "italic": ("i", text_node.text),
+        "code": ("code", text_node.text),
+        "link": ("a", text_node.text, {"href": text_node.url}),
+        "image": ("img", "", {"src": text_node.url, "alt": text_node.text}),
+    }
+
+    if text_node.text_type not in tag_map:
+        raise ValueError(f"Unsupported text type: {text_node.text_type}")
+
+    tag, content, *props = tag_map[text_node.text_type]
+    return LeafNode(tag, content, props[0] if props else None)

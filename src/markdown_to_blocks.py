41e5fe8e1362ec9c01b4b92a7482d41c4d5affd5
Jcurt4 <joe.curtis.iv@gmail.com>
2024-10-12 15:30:17 -0600

    doing tests on block to block type.  Failing on ordered lists

--- src/markdown_to_blocks.py
+++ src/markdown_to_blocks.py
@@ -1,5 +1,48 @@
+import re
 
+BLOCK_TYPE_PARAGRAPH = 'paragraph'
+BLOCK_TYPE_HEADER = 'header'
+BLOCK_TYPE_UNORDERED_LIST = 'unordered_list'
+BLOCK_TYPE_ORDERED_LIST = 'ordered_list'
+BLOCK_TYPE_QUOTE = 'quote'
+BLOCK_TYPE_CODE = 'code'
 
 def markdown_to_blocks(markdown):
     filtered_blocks = [block.strip() for block in markdown.split('\n\n') if block.strip()]
     return filtered_blocks
+
+
+def block_to_block_type(block):
+    if block.startswith(
+        ('# ', '## ', '### ', '#### ', '##### ', '###### ') 
+    ):
+        return BLOCK_TYPE_HEADER
+    
+    elif block.startswith('* ') or block.startswith('- '):
+        lines = block.split('\n')
+        if all(line.startswith('* ') or line.startswith('- ') for line in lines):
+            return BLOCK_TYPE_UNORDERED_LIST
+        
+    elif block.startswith('```') and block.endswith('```'):
+        return BLOCK_TYPE_CODE
+    
+    elif block.startswith('>'):
+        lines = block.split('\n')
+        if all(line.startswith('> ') for line in lines):
+            return BLOCK_TYPE_QUOTE
+        
+    elif block.startswith('1. '):
+        lines = block.split('\n')
+        pattern = re.compile(r'^(\d+)\.\s', re.MULTILINE)
+        matches = pattern.findall(block)
+        is_ordered = True
+        if len(lines) == len(matches):
+            for i, num in enumerate(matches):
+                if int(num) != i+1:
+                    is_ordered = False
+                    break
+            if is_ordered:
+                return BLOCK_TYPE_ORDERED_LIST
+        return BLOCK_TYPE_PARAGRAPH
+
+    return BLOCK_TYPE_PARAGRAPH
--- src/test_markdown_to_blocks.py
+++ src/test_markdown_to_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_to_blocks import markdown_to_blocks
+from markdown_to_blocks import markdown_to_blocks, block_to_block_type, BLOCK_TYPE_PARAGRAPH, BLOCK_TYPE_HEADER, BLOCK_TYPE_UNORDERED_LIST, BLOCK_TYPE_ORDERED_LIST, BLOCK_TYPE_QUOTE, BLOCK_TYPE_CODE
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -8,9 +8,9 @@ class TestMarkdownToBlocks(unittest.TestCase):
 # Hello world! 
 
 This is a paragraph to test my newest funcion.  I need to make sure that is is working correctly.
-this means this is still part of the paragraph.  
+this means this is still part of the paragraph.
 so it this.
-and this. 
+and this.
 
 *but this will be an unordered list item
 *another one
@@ -19,12 +19,94 @@ and this.
         blocks = markdown_to_blocks(markdown)
         expected = [
             "# Hello world!",
-            "This is a paragraph to test my newest funcion.  I need to make sure that is is working correctly. this means this is still part of the paragraph.  so it this. and this.",
+            "This is a paragraph to test my newest funcion.  I need to make sure that is is working correctly.\nthis means this is still part of the paragraph.\nso it this.\nand this.",
             "*but this will be an unordered list item\n*another one\n*dj khaled",
         ]
         self.assertEqual(blocks, expected)
 
+    def test_markdown_to_blocks_empty(self):
+        markdown = ""
+        blocks = markdown_to_blocks(markdown)
+        expected = []
+        self.assertEqual(blocks, expected)
 
+    def test_markdown_to_blocks_no_newlines(self):
+        markdown = "# Hello world!"
+        blocks = markdown_to_blocks(markdown)
+        expected = ["# Hello world!"]
+        self.assertEqual(blocks, expected)
+    
+    def test_with_multiple_newlines(self):
+        markdown = """
+        this is a test with multiple new lines.
+
+
+
+        between the blocks."""
+        blocks = markdown_to_blocks(markdown)
+        expected = ["this is a test with multiple new lines.", "between the blocks."]
+        self.assertEqual(blocks, expected)
+
+    def test_with_leading_and_trailing_blank_space(self):
+        markdown = """
+                     this is a test with a bunch of leading and trailing white space       
+
+
+
+
+                            """
+        blocks = markdown_to_blocks(markdown)
+        expected = ["this is a test with a bunch of leading and trailing white space"]
+        self.assertEqual(blocks, expected)
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_basic_function(self):
+        block = "this is a paragraph"
+        block_type = block_to_block_type(block)
+        expected = BLOCK_TYPE_PARAGRAPH
+        self.assertEqual(block_type, expected)
+
+    def test_header(self):
+        block = "# this is a header"
+        block_type = block_to_block_type(block)
+        expected = BLOCK_TYPE_HEADER
+        self.assertEqual(block_type, expected)
+    
+    def test_unordered_list(self):
+        block = "* this is an unordered list"
+        block_type = block_to_block_type(block)
+        expected = BLOCK_TYPE_UNORDERED_LIST
+        self.assertEqual(block_type, expected)
+
+    def test_ordered_list(self):
+        block = """1. this is an ordered list
+        2. this is the second item
+        3. this is the third item"""
+        block_type = block_to_block_type(block)
+        expected = BLOCK_TYPE_ORDERED_LIST
+        self.assertEqual(block_type, expected)
+    
+    def test_quote(self):
+        block = "> this is a quote"
+        block_type = block_to_block_type(block)
+        expected = BLOCK_TYPE_QUOTE
+        self.assertEqual(block_type, expected)
+    
+    def test_code(self):
+        block = "```this is a code block```"
+        block_type = block_to_block_type(block)
+        expected = BLOCK_TYPE_CODE
+        self.assertEqual(block_type, expected)
+
+    def test_ordered_with_wrong_order(self):
+        block = """1. this is an ordered list
+        2. this is the second item
+        4. this is the fourth item"""
+        block_type = block_to_block_type(block)
+        expected = BLOCK_TYPE_PARAGRAPH
+        self.assertEqual(block_type, expected)
+    
 
 
 if __name__ == '__main__':
